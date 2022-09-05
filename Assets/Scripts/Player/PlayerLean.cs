1a1b9b9cce8b699dd16a15eb47d0d091b7c1eb98
George <george.h.vazquez@gmail.com>
2022-09-05 12:34:09 -0400

    Updated leaning. Added a new input system
    I found that Unity has a new input system package that seems a little more robust than the default. Implementing it means redoing the FPS controller. This should be top priority in order to get the controls working properly from here on out.

--- Assets/Scripts/Player/PlayerLean.cs
+++ Assets/Scripts/Player/PlayerLean.cs
@@ -4,36 +4,95 @@ using UnityEngine;
 
 public class PlayerLean : MonoBehaviour
 {
-    public Transform pivot;
+    // ----    This is the old leaning code.                                                    ----
+    // ----    The disabled lines are for a leaning mechanic that is more of a sharper lean.    ----
+    // ----    While the active code makes the leaning smoother.                                ----
 
-    public float speed = 100f;
-    public float maxAngle = 20f;
-    private float currentAngle = 0f;
 
-    void Awake()
+    // public Transform pivot;
+
+    // public float speed = 100f;
+    // public float maxAngle = 20f;
+    // private float currentAngle = 0f;
+
+    // void Awake()
+    // {
+    //     if (pivot == null && transform.parent != null)
+    //     {
+    //         pivot = transform.parent;
+    //     }
+    // }
+
+
+    // void Update()
+    // {
+    //     if (Input.GetKey(KeyCode.Q))
+    //     {
+    //         currentAngle = Mathf.MoveTowardsAngle(currentAngle, maxAngle, speed * Time.deltaTime);
+    //     }
+    //     else if (Input.GetKey(KeyCode.E))
+    //     {
+    //         currentAngle = Mathf.MoveTowardsAngle(currentAngle, -maxAngle, speed * Time.deltaTime);
+    //     }
+    //     else
+    //     {
+    //         currentAngle = Mathf.MoveTowardsAngle(currentAngle, 0f, speed * Time.deltaTime);
+    //     }
+
+    //     pivot.transform.localRotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+    // }
+
+    public Transform leanPivot;
+    private float currentLean;
+    private float targetLean;
+    public float leanAngle;
+    public float leanSmoothing;
+    private float leanVelocity;
+
+    private bool isLeaningLeft;
+    private bool isLeaningRight;
+
+
+    private void CalculateLeaning()
     {
-        if (pivot == null && transform.parent != null)
+        if (isLeaningLeft)
         {
-            pivot = transform.parent;
+            targetLean = leanAngle;
         }
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Q))
+        else if (isLeaningRight)
         {
-            currentAngle = Mathf.MoveTowardsAngle(currentAngle, maxAngle, speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            currentAngle = Mathf.MoveTowardsAngle(currentAngle, -maxAngle, speed * Time.deltaTime);
+            targetLean = -leanAngle;
         }
         else
         {
-            currentAngle = Mathf.MoveTowardsAngle(currentAngle, 0f, speed * Time.deltaTime);
+            targetLean = 0;
         }
 
-        pivot.transform.localRotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+        currentLean = Mathf.SmoothDamp(currentLean, targetLean, ref leanVelocity, leanSmoothing);
+        leanPivot.localRotation = Quaternion.Euler(new Vector3(0, 0, currentLean));
+    }
+
+    void Update()
+    {
+        CalculateLeaning();
+        Debug.Log("Left = " + isLeaningLeft + " " + "Right = " + isLeaningRight + " " + "Target lean = " + targetLean);
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            isLeaningLeft = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            isLeaningRight = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            isLeaningLeft = false;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            isLeaningRight = false;
+        }
+
     }
 }
