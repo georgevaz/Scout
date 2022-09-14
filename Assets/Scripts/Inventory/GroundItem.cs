f0528b2a1c743dedb9d7a86c1f435d23ae2b524a
George Vazquez <george.h.vazquez@gmail.com>
2022-09-13 22:36:29 -0400

    Added serialization functions to ground items
    This will be useful to make sure we are using the correct meshes per item without having to manually implementing them every time we would need to put them into the scene. The item data will have a reference to the mesh we want to use for it.

--- Assets/Scripts/Inventory/GroundItem.cs
+++ Assets/Scripts/Inventory/GroundItem.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class GroundItem : MonoBehaviour
+public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 {
     public ItemObject item;
+
+    public void OnAfterDeserialize()
+    {
+
+    }
+    public void OnBeforeSerialize()
+    {
+        GetComponentInChildren<MeshFilter>().mesh = item.prefab.GetComponent<MeshFilter>().sharedMesh;
+        EditorUtility.SetDirty(GetComponentInChildren<MeshFilter>());
+        GetComponentInChildren<MeshRenderer>().material = item.prefab.GetComponent<MeshRenderer>().sharedMaterial;
+        EditorUtility.SetDirty(GetComponentInChildren<MeshRenderer>());
+        transform.GetChild(0).transform.localScale = item.prefab.transform.localScale;
+        EditorUtility.SetDirty(transform.GetChild(0).transform);
+
+    }
 }
